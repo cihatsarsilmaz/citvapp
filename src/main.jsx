bf49c34d873842060dddaa722200e497eddf36ba
cihatsarsilmaz <45016062+cihatsarsilmaz@users.noreply.github.com>
2026-09-02 03:34:08 +0300

    feat: payhint satırı sahneye

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 import "./slot5.css";
+import "./payhint.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
